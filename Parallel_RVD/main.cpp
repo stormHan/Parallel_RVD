e9fb80974422563635e70bb639d5fcda35378c4f
stormHan <stormHan1205@gmail.com>
2016-06-03 16:20:45 +0800

    add main entrance

---
+++ Parallel_RVD/main.cpp
@@ -0,0 +1,34 @@
+/*
+
+	Parallel compute the Restricted Voronoi Diagram
+
+
+*/
+
+#include "Command_line.h"
+#include "Mesh_io.h"
+#include "Mesh.h"
+
+
+int main(int argc, char** argv)
+{
+	using namespace P_RVD;
+
+	std::vector<std::string> filenames;
+	if (!Cmd::parse_argu(argc, argv, filenames))
+	{
+		fprintf(stderr, "cannot parse the argument into filenames!");
+		return -1;
+	}
+	
+	std::string mesh_filename = filenames[0];
+	std::string points_filename = filenames[0];
+	std::string output_filename;
+	if (filenames.size() >= 2) {
+		points_filename = filenames[1];
+	}
+	output_filename = (filenames.size() == 3) ? filenames[2] : "out.eobj";
+
+	
+	return 0;
+}
