aac2731f61c0acababcdb2f7771e4fef268baf4a
stormHan <stormHan1205@gmail.com>
2016-06-03 16:20:10 +0800

    add Command line

---
+++ Parallel_RVD/Command_line.cpp
@@ -0,0 +1,29 @@
+
+#include "Command_line.h"
+
+namespace P_RVD
+{
+	namespace Cmd
+	{
+		bool parse_argu(int argument_nb, char** argument, std::vector<std::string>& filenames)
+		{
+			if (argument_nb <= 1)
+			{
+				fprintf(stderr, "no argument pointed to filename!");
+				return false;
+			}
+
+			if (filenames.size() >= 1)
+			{
+				fprintf(stderr, "already have filenames!");
+				return false;
+			}
+			for (int i = 1; i < argument_nb && i <= 3; ++i)
+			{
+				filenames.push_back(argument[i]);
+			}
+
+			return true;
+		}
+	}
+}
