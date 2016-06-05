9f401b592830e2e4597eb6fbb7eb1f5fdda58ed8
stormHan <stormHan1205@gmail.com>
2016-06-05 22:06:08 +0800

    add mesh_load

--- Parallel_RVD/main.cpp
+++ Parallel_RVD/main.cpp
@@ -29,6 +29,14 @@ int main(int argc, char** argv)
 	}
 	output_filename = (filenames.size() == 3) ? filenames[2] : "out.eobj";
 
+	Mesh M_in, M_out, points_in;
+	FileType mesh_type = OBJfile;
 	
+	if (!mesh_load(filenames[0], M_in))
+	{
+		fprintf(stderr, "cannot load Mesh!");
+		return -1;
+	}
+
 	return 0;
 }
