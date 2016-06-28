443475f0debe3deeb22fbbf0138448f333113bd1
stormHan <stormHan1205@gmail.com>
2016-06-28 21:30:41 +0800

    update clip_by_plane func

--- Parallel_RVD/Polygon.cpp
+++ Parallel_RVD/Polygon.cpp
@@ -56,7 +56,7 @@ namespace P_RVD
 		//The predecessor of the first vertex is the last vertex
 		t_index prev_index_vertex = getVertex_nb() - 1;
 		const Vertex* prev_vertex = &(getVertex(prev_index_vertex));
-		const Vector3d prev_vertex_position = prev_vertex->getPosition();
+		Vector3d prev_vertex_position = prev_vertex->getPosition();
 		
 		//then we compute prev_vertex_position "cross" n 
 		//prev_l = prev_vertex_position . n
@@ -68,11 +68,74 @@ namespace P_RVD
 		for (t_index k = 0; k < getVertex_nb(); ++k)
 		{
 			const Vertex* vertex = &(getVertex(k));
-			const Vector3d vertex_position = vertex->getPosition();
+			Vector3d vertex_position = vertex->getPosition();
 
 			double l = vertex_position.cross(position_i - position_j);
 			
+			//We compute:
+			// side1(pi, pj, q) = sign(2*q.n - n.m) = sign(2*l - d)
 			P_RVD::Sign status = P_RVD::geo_sgn(2.0 * l - d);
+
+			// If status of edge extremities differ,
+			// then there is an intersection.
+			if (status != prev_status && (prev_status) != 0)
+			{
+				// create the intersection and update the Polygon
+				Vertex I;
+				Vector3d temp_position;
+
+
+				//compute the position and weight
+				double denom = 2.0 * (prev_l - l);
+				double lambda1, lambda2;
+
+				// Shit happens ! [Forrest Gump]
+				if (::fabs(denom) < 1e-20) {
+					lambda1 = 0.5;
+					lambda2 = 0.5;
+				}
+				else {
+					lambda1 = (d - 2.0 * l) / denom;
+					// Note: lambda2 is also given
+					// by (2.0*l2-d)/denom
+					// (but 1.0 - lambda1 is a bit
+					//  faster to compute...)
+					lambda2 = 1.0 - lambda1;
+				}
+
+				temp_position.x = lambda1 * prev_vertex_position.x + lambda2 * vertex_position.x;
+				temp_position.y = lambda1 * prev_vertex_position.y + lambda2 * vertex_position.y;
+				temp_position.z = lambda1 * prev_vertex_position.z + lambda2 * vertex_position.z;
+
+				//Set the position of Vertex
+				I.setPosition(temp_position);
+
+				//Set the weight of Veretex
+				I.setWeight(lambda1 * prev_vertex->getWeight() + lambda2 * vertex->getWeight());
+
+				//???? »¹Ã»Åª¶®
+				if (status > 0)
+				{
+					I.copy_edge_from(*prev_vertex);
+					I.setSeed(signed_t_index(_j));
+				}
+				else
+				{
+					I.setEdgeType(Intersection);
+					I.setSeed(vertex->getSeed());
+				}
+				_target.add_vertex(I);
+			}
+
+			if (status > 0)
+			{
+				_target.add_vertex(*vertex);
+			}
+			prev_vertex = vertex;
+			prev_vertex_position = vertex_position;
+			prev_status = status;
+			prev_l = l;
+			prev_index_vertex = k;
 		}
 
 		return;
