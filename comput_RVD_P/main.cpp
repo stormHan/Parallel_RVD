1658fea6d57182b750dab9239b72969ee47b6ab6
JWhan <stormhan1205@gmail.com>
2016-08-22 21:07:06 +0800

    push the main func

--- comput_RVD_P/main.cpp
+++ comput_RVD_P/main.cpp
@@ -4,15 +4,18 @@
 
 	with Cuda to accelerate the program
 
-	starting time : 2017.7.25
+	starting time : 2016.7.25
 */
 
 #include "Command_line.h"
 
+
+using namespace Parallel_RVD;
+
 int main(int argc, char** argv)
 {
 	std::vector<std::string> filenames;
-
+	
 
 	return 0;
 }
