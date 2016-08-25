41c0eb18894cdee2c2e4757e4cf396632ccf8612
JWhan <stormhan1205@gmail.com>
2016-08-25 17:21:24 +0800

    add mesh class

---
+++ comput_RVD_P/Mesh.cpp
@@ -0,0 +1,79 @@
+#include "Mesh.h"
+
+namespace P_RVD
+{
+	/*--------------------- Line -----------------------*/
+	Line::Line(t_index _begin, t_index _end)
+	{
+		m_begin = _begin;
+		m_end = _end;
+	}
+
+	/*--------------------- Facet-----------------------*/
+
+	Facet::Facet(t_index _v1, t_index _v2, t_index _v3)
+	{
+		m_v1 = _v1;
+		m_v2 = _v2;
+		m_v3 = _v3;
+	}
+
+	/*----------------- Mesh Vertices ------------------*/
+
+	Vector3d MeshVertices::getPoint(t_index _nb) const
+	{
+		return m_Vertices[_nb];
+	}
+
+	void MeshVertices::addPoint(Vector3d _newpoint)
+	{
+		m_nb++;
+		m_Vertices.push_back(_newpoint);
+	}
+
+	MeshVertices::MeshVertices(Mesh& _mesh)
+	{
+		m_Vertices = _mesh.meshVertices.m_Vertices;
+		m_nb = _mesh.meshVertices.m_nb;
+	}
+
+	/*----------------- Mesh Edges --------------------*/
+
+	MeshEdges::MeshEdges(Mesh& _mesh)
+	{
+		m_Edges = _mesh.meshEdges.m_Edges;
+	}
+
+	/*----------------- Mesh Facets -------------------*/
+
+	MeshFacets::MeshFacets(Mesh& _mesh)
+	{
+		m_Facets = _mesh.meshFacets.m_Facets;
+		m_nb = _mesh.meshFacets.m_nb;
+	}
+
+	void MeshFacets::addFacet(const Facet _f)
+	{
+		m_nb++;
+		m_Facets.push_back(_f);
+	}
+
+	void  MeshFacets::addFacet(t_index _v1, t_index _v2, t_index _v3)
+	{
+		m_nb++;
+		m_Facets.push_back(Facet(_v1, _v2, _v3));
+	}
+
+	Facet MeshFacets::getFacet(t_index _t) const
+	{
+		return m_Facets[_t];
+	}
+	/*--------------------- Mesh ----------------------*/
+	Mesh::Mesh()
+		: meshVertices(*this),
+		meshEdges(*this),
+		meshFacets(*this)
+	{
+
+	}
+}
---
+++ comput_RVD_P/Mesh.h
@@ -0,0 +1,128 @@
+/*
+***Data Structure Mesh***
+
+Store the infomation of
+Input	Model;
+Input	Points;
+Output	Model;
+
+Include:
+Mesh Vertex;
+Mesh Edges;
+Mesh Facets;
+
+*/
+
+#ifndef H_MESH_H
+#define H_MESH_H
+
+#include "math_3d.h"
+#include "Common.h"
+
+#include <vector>
+
+namespace P_RVD
+{
+	class Line
+	{
+	public:
+		Line(t_index _begin, t_index _end);
+
+	private:
+		t_index m_begin;
+		t_index m_end;
+	};
+
+	class Facet
+	{
+	public:
+		Facet(t_index _v1, t_index _v2, t_index _v3);
+
+		t_index m_v1;
+		t_index m_v2;
+		t_index m_v3;
+	};
+
+	class Mesh;
+	/*----------------- Mesh Vertices ------------------*/
+	class MeshVertices
+	{
+	public:
+
+		friend class Points;
+
+		MeshVertices(Mesh& _mesh);
+
+
+		/*
+		get Point Position via indice
+		*/
+
+		Vector3d getPoint(t_index _nb) const;
+
+		/*
+		add a Point to m_Vertices
+		*/
+		void addPoint(Vector3d _newpoint);
+
+	private:
+		std::vector<Vector3d> m_Vertices;
+		unsigned int m_nb = 0;
+
+	};
+
+	class MeshEdges
+	{
+	public:
+		MeshEdges(Mesh& _mesh);
+
+	private:
+		std::vector<Line> m_Edges;
+	};
+
+	class MeshFacets
+	{
+	public:
+		MeshFacets(Mesh& _mesh);
+
+		/*
+		add a Facet to m_Facets
+		*/
+		void addFacet(const Facet _f);
+
+		/*
+		add a Facet via three indice of m_Vertices
+		*/
+		void addFacet(t_index _v1, t_index _v2, t_index _v3);
+
+		/*
+		get the Facet infomation via index
+		*/
+		Facet getFacet(t_index _t) const;
+
+		/*
+		get the number of facets
+		*/
+		unsigned int getFacetsNumber() const { return m_nb; }
+
+	private:
+		std::vector<Facet> m_Facets;
+		unsigned int m_nb;
+	};
+
+
+
+	class Mesh
+	{
+	public:
+		MeshVertices	meshVertices;
+		MeshEdges		meshEdges;
+		MeshFacets		meshFacets;
+
+
+		Mesh();
+	private:
+	};
+
+}
+#endif /* H_MESH_H */
