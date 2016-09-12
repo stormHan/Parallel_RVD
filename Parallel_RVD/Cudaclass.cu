761c42169ced5d1e097f82ab130c0a9515d0e4d3
JWhan <stormhan1205@gmail.com>
2016-09-12 21:20:05 +0800

    add core clip func

--- Parallel_RVD/Cudaclass.cu
+++ Parallel_RVD/Cudaclass.cu
@@ -91,49 +91,22 @@ void findNearestPoints(double3 centriod, double* seeds_pointer, int seeds_nb, in
 	{
 		nearest_points[i] = index[i];
 	}
+
+	free(distance_from_centroid);
+	free(index);
 }
 
 
 /*
-	device function
-	compute the intersection of a facet and a seed(a cell)
+device function
 
-	input:
+compute the intersection(stored in a polygon) clipped by a bisector defined by seed i and j
 
+input : polygon ping and its number
+output: polygon pong and its number
 */
 __device__
-void intersection_clip_facet(double* polygon_pointer, int vertex_nb, int current_seed, double* seeds_pointer, int seeds_nb)
-{
-	double3 current_seed_pos = { seeds_pointer[current_seed * 3 + 0],
-		seeds_pointer[current_seed * 3 + 1],
-		seeds_pointer[current_seed * 3 + 2] };
-
-	int* seeds_neighbors = (int*)malloc(sizeof(int) * 20);
-	findNearestPoints(current_seed_pos, seeds_pointer, seeds_nb, seeds_neighbors, 20);
-
-	//set a buffer pointer to store the polygon
-	double* polygon_buffer = (double*)malloc(sizeof(double) * 10 * 4);
-	int polygon_buffer_nb = 0;
-
-	for (int i = 0; i < 20; ++i)
-	{
-		int j = seeds_neighbors[i];
-
-		clip_by_plane(polygon_pointer, vertex_nb, polygon_buffer, polygon_buffer_nb, current_seed, j, seeds_pointer, seeds_nb);
-		swap_polygons(polygon_pointer, vertex_nb, polygon_buffer, polygon_buffer_nb);
-	}
-}
-
-/*
-	device function
-	
-	compute the intersection(stored in a polygon) clipped by a bisector defined by seed i and j
-
-	input : polygon ping and its number
-	output: polygon pong and its number
-*/
-__device__
-void clip_by_plane(double* ping, int ping_nb, double* pong, int pong_nb, int seed_i, int seed_j, double* seeds_pointer, int seeds_nb)
+void clip_by_plane(double* ping, int& ping_nb, double* pong, int& pong_nb, int seed_i, int seed_j, double* seeds_pointer, int seeds_nb)
 {
 	//reset the pong
 	pong_nb = 0;
@@ -149,17 +122,99 @@ void clip_by_plane(double* ping, int ping_nb, double* pong, int pong_nb, int see
 	// normal vector of the bisector [i, j]
 	// and m the middle point of the bisector.
 	double d;
-	d = dot(position_i,position_j)
+	d = dot(add(position_i, position_j), sub(position_i, position_j));
 
+	//The predecessor of the first vertex is the last vertex
+	int prev_index_vertex = ping_nb - 1;
+
+	//get the position data
+	double4 prev_vertex = { ping[prev_index_vertex * 4 + 0],
+		ping[prev_index_vertex * 4 + 1],
+		ping[prev_index_vertex * 4 + 2],
+		ping[prev_index_vertex * 4 + 3] };
+
+	double3 prev_vertex_position = { prev_vertex.x, prev_vertex.y, prev_vertex.z };
+
+	//then we compute prev_vertex_position "cross" n 
+	//prev_l = prev_vertex_position . n
+
+	double prev_l = dot(prev_vertex_position, sub(position_i, position_j));
+
+	int prev_status = sgn(2.0 * prev_l - d);
+
+	//traverse the Vertex in this Polygon
+	for (int k = 0; k < ping_nb; ++k)
+	{
+		double4 vertex = { ping[k * 4 + 0], ping[k * 4 + 1], ping[k * 4 + 2], ping[k * 4 + 3] };
+		double3 vertex_position = { vertex.x, vertex.y, vertex.z };
+
+		double l = dot(vertex_position, sub(position_i, position_j));
+		int status = sgn(2.0 * l - d);
+
+		//If status of edge extremities differ,
+		//then there is an intersection.
+		if (status != prev_status && (prev_status) != 0)
+		{
+			// create the intersection and update the Polyon
+			double4 I;
+
+			//compute the position and weight
+			double denom = 2.0 * (prev_l - l);
+			double lambda1, lambda2;
+
+			// Shit happens!
+			if (m_fabs(denom) < 1e-20)
+			{
+				lambda1 = 0.5;
+				lambda2 = 0.5;
+			}
+			else
+			{
+				lambda1 = (d - 2.0 * l) / denom;
+				// Note: lambda2 is also given
+				// by (2.0*l2-d)/denom
+				// (but 1.0 - lambda1 is a bit
+				//  faster to compute...)
+				lambda2 = 1.0 - lambda1;
+			}
+
+			//Set the Position of Vertex
+			I.x = lambda1 * prev_vertex_position.x + lambda2 * vertex_position.x;
+			I.y = lambda1 * prev_vertex_position.y + lambda2 * vertex_position.y;
+			I.z = lambda1 * prev_vertex_position.z + lambda2 * vertex_position.z;
+
+			//Set the Weight of Vertex
+			I.w = (lambda1 * prev_vertex.w + lambda2 * vertex.w);
+
+			//add I to pong
+			pong[pong_nb * 4 + 0] = I.x;
+			pong[pong_nb * 4 + 1] = I.y;
+			pong[pong_nb * 4 + 2] = I.z;
+			pong[pong_nb * 4 + 3] = I.w;
+			pong_nb++;
+
+		}
+
+		if (status > 0)
+		{
+			//add vertex to pong
+			pong[pong_nb * 4 + 0] = vertex.x;
+			pong[pong_nb * 4 + 1] = vertex.y;
+			pong[pong_nb * 4 + 2] = vertex.z;
+			pong[pong_nb * 4 + 3] = vertex.w;
+			pong_nb++;
+		}
+	}
+	return;
 }
 
 /*
-	device function
-	swap the ping and pong to make sure 
-	ping store the result;
+device function
+swap the ping and pong to make sure
+ping store the result;
 */
 __device__
-void swap_polygons(double* ping, int ping_nb, double* pong, int pong_nb)
+void swap_polygons(double* ping, int& ping_nb, double* pong, int& pong_nb)
 {
 	//!!! can be accerate
 	//不确定是否可以使用memset memcpy
@@ -177,6 +232,78 @@ void swap_polygons(double* ping, int ping_nb, double* pong, int pong_nb)
 	}
 }
 
+
+/*
+	device function
+	compute the intersection of a facet and a seed(a cell)
+
+	input:
+
+*/
+__device__
+void intersection_clip_facet(double* polygon_pointer, int& vertex_nb, int current_seed, double* seeds_pointer, int seeds_nb, int* test_index, double* test_polygon, int tid, int _i)
+{
+	double3 current_seed_pos = { seeds_pointer[current_seed * 3 + 0],
+		seeds_pointer[current_seed * 3 + 1],
+		seeds_pointer[current_seed * 3 + 2] };
+
+	int* seeds_neighbors = (int*)malloc(sizeof(int) * 20);
+	findNearestPoints(current_seed_pos, seeds_pointer, seeds_nb, seeds_neighbors, 20);
+
+	////set a buffer pointer to store the polygon
+	double* polygon_buffer = (double*)malloc(sizeof(double) * 10 * 4);
+	int polygon_buffer_nb = 0;
+
+	for (int i = 0; i < 20; ++i)
+	{
+		int j = seeds_neighbors[i];
+		int q;
+		if (tid == 0 && _i == 0 && i == 19)
+		{
+			for (q = 0; q < vertex_nb * 4; ++q)
+			{
+				test_polygon[6 + q] = polygon_pointer[q];
+			}
+		}
+
+		if (current_seed != j)
+		{ 
+			if (tid == 0 && _i == 0 && i == 19)
+			{
+				test_index[42] = j;
+				test_index[43] = vertex_nb;
+				
+			}
+			clip_by_plane(polygon_pointer, vertex_nb, polygon_buffer, polygon_buffer_nb, current_seed, j, seeds_pointer, seeds_nb);
+			swap_polygons(polygon_pointer, vertex_nb, polygon_buffer, polygon_buffer_nb);
+			if (tid == 0 && _i == 0 && i == 19)
+			{
+				test_index[44] = vertex_nb;
+				test_index[45] = polygon_buffer_nb;
+				for (int p = 0; p < vertex_nb * 4; ++p)
+				{
+					test_polygon[101 + p] = polygon_pointer[p];
+				}
+			}
+		}
+	}
+	if (tid == 0 && _i == 0)
+	{
+		test_polygon[3] = current_seed_pos.x;
+		test_polygon[4] = current_seed_pos.y;
+		test_polygon[5] = current_seed_pos.z;
+		for (int i = 0; i < 20; ++i)
+		{
+			test_index[i + 21] = seeds_neighbors[i];
+		}
+		test_index[41] = current_seed;
+
+	
+	}
+
+	free(seeds_neighbors);
+	free(polygon_buffer);
+}
 
 /*
 	a thread to compute the RVD in parallel way
@@ -217,6 +344,8 @@ __global__ void compute_RVD(double* seeds_pointer, int seeds_nb,
 
 		findNearestPoints(centriod, seeds_pointer, seeds_nb, nearest_points, 20);
 		
+		double* polygon = (double*)malloc(sizeof(double) * 10 * 4);
+		int vertex_nb;
 		/*
 			get the nearest 20 points' index
 			clip the facet
@@ -225,8 +354,8 @@ __global__ void compute_RVD(double* seeds_pointer, int seeds_nb,
 		{
 			int current_seed = nearest_points[i];
 
-			double* polygon = (double*)malloc(sizeof(double) * 10 * 4);
-			int vertex_nb = 3;
+			vertex_nb = 3;
+			
 			//intialize the polygon with the 3 vertex of the current facet
 			/*
 				polygon pointer can be apart by several vertex
@@ -237,7 +366,26 @@ __global__ void compute_RVD(double* seeds_pointer, int seeds_nb,
 			polygon[4] = v2.x; polygon[5] = v2.y; polygon[6] = v2.z; polygon[7] = 1.0;
 			polygon[8] = v3.x; polygon[9] = v3.y; polygon[10] = v3.z; polygon[11] = 1.0;
 
+			intersection_clip_facet(polygon, vertex_nb, current_seed, seeds_pointer, seeds_nb,
+				test_index, test_dis, tid, i);
 
+			if (tid == 0 && i == 0)
+			{
+				test_index[20] = current_seed;
+
+				/*for (int j = 0; j < vertex_nb * 4; ++j)
+				{
+					test_dis[j] = polygon[j];
+				}*/
+
+				double3 current_seed_pos = { seeds_pointer[current_seed * 3 + 0],
+					seeds_pointer[current_seed * 3 + 1],
+					seeds_pointer[current_seed * 3 + 2] };
+
+				test_dis[0] = current_seed_pos.x;
+				test_dis[1] = current_seed_pos.y;
+				test_dis[2] = current_seed_pos.z;
+			}
 		}
 		
 
@@ -253,6 +401,9 @@ __global__ void compute_RVD(double* seeds_pointer, int seeds_nb,
 				test_index[i] = nearest_points[i];
 			}
 		}
+
+		free(nearest_points);
+		free(polygon);
 	}
 }
 
@@ -303,16 +454,36 @@ extern "C" void runCuda(double* host_seeds_pointer, double* host_mesh_vertex_poi
 	checkCudaErrors(cudaMemcpy(host_test_cen, dev_test_center, sizeof(double) * 3, cudaMemcpyDeviceToHost));
 	checkCudaErrors(cudaMemcpy(host_test_index, dev_test_index, sizeof(int) * points_nb, cudaMemcpyDeviceToHost));
 
-	printf("center position : %.17lf, %.17lf, %.17lf \n", host_test_cen[0], host_test_cen[1], host_test_cen[2]);
-	for (int i = 0; i < points_nb; ++i)
-	{
-		printf("%d  : %.16lf  \n",i, host_test_dis[i]);
-	}
 
+	printf("Thread 1:\n");
+	printf("center position : %.17lf, %.17lf, %.17lf \n", host_test_cen[0], host_test_cen[1], host_test_cen[2]);
+	printf("nearest 20 points' index:\n");
 	for (int i = 0; i < 20; ++i)
 	{
 		printf("%d  : %d  \n",i, host_test_index[i]);
 	}
+	printf("current seed : %d\n", host_test_index[41]);
+	printf("current seed position : %.17lf, %.17lf, %.17lf \n", host_test_dis[3], host_test_dis[4], host_test_dis[5]);
 
-	printf("successfully running!");
+	printf("the 20 seeds near the current seed\n");
+	for (int i = 0; i < 20; ++i)
+	{
+		printf("%d  : %d  \n",i, host_test_index[i + 21]);
+	}
+
+	printf(" %d 和 %d 确定平面\n", host_test_index[41], host_test_index[42]);
+	printf("原polygon:\n");
+	printf("vertex number : %d\n", host_test_index[43]);
+
+	for (int i = 0; i < host_test_index[43]; ++i)
+	{
+		printf("x : %.17lf, y: %.17lf, z : %.17lf, w : %.6lf\n\n", host_test_dis[6 + i * 4 + 0], host_test_dis[6 + i * 4 + 1], host_test_dis[6 + i * 4 + 2], host_test_dis[6 + i * 4 + 3]);
+	}
+	printf("现polygon:\n");
+	printf("vertex number : %d   buffer vertex number : %d\n", host_test_index[44], host_test_index[45]);
+	for (int i = 0; i < host_test_index[44]; ++i)
+	{
+		printf("x : %.17lf, y: %.17lf, z : %.17lf, w : %.6lf\n\n", host_test_dis[101 + i * 4 + 0], host_test_dis[101 + i * 4 + 1], host_test_dis[101 + i * 4 + 2], host_test_dis[101 + i * 4 + 3]);
+	}
+	printf("successfully running!\n");
 }
